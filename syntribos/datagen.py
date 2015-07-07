c0e35063839f0f70356eee9e82462f5c3b67a84f
step6829 <stephen.lowrie@rackspace.com>
2015-07-07 14:12:24 -0500

    Added ignore property to datagen
    Allows the user to pass in a string that will cause the data generator to ignore it.
    
    Example:
    	Input XML:
    		test = """<example><IGNORE_MEfoo>asdf</foo><bar>fdsa</bar></example>"""
    	Call:
    		MyDataGenerator("Example", test, "IGNORE_ME")
    	Output XML:
    		<example><foo>asdf</foo><bar>FUZZED DATA HERE</bar></example>

--- syntribos/datagen.py
+++ syntribos/datagen.py
@@ -7,25 +7,29 @@ from cafe.drivers.unittest.datasets import DatasetList
 class DynamicDataGen(DatasetList):
     strings = None
 
-    def __init__(self, name, data):
+    def __init__(self, name, data, ignore_var):
         for stri in self.strings:
             ctr = 0
             if isinstance(data, dict):
-                for dic in self._build_combinations(name, stri, data):
+                for dic in self._build_combinations(
+                        name, stri, data, ignore_var):
                     self.append_new_dataset(
                         "{0}-{1}-{2}".format(self.test_name, name, ctr), {
-                            "model": json.dumps(dic)})
+                            "model": json.dumps(dic).replace(
+                                ignore_var, "")})
                     ctr += 1
             elif isinstance(data, ElementTree.Element):
-                for element in self._build_xml_combinations(name, stri, data):
+                for element in self._build_xml_combinations(
+                        name, stri, data, ignore_var):
                     self.append_new_dataset(
                         "{0}-{1}-{2}".format(self.test_name, name, ctr), {
-                            "model": ElementTree.tostring(element)})
+                            "model": ElementTree.tostring(element).replace(
+                                ignore_var, "")})
                     ctr += 1
             else:
                 raise TypeError("Format not recognized!")
 
-    def _build_combinations(self, name, stri, dic):
+    def _build_combinations(self, name, stri, dic, ignore_var):
         for key, val in dic.iteritems():
             if isinstance(val, dict):
                 for ret in self._build_combinations(name, stri, val):
@@ -37,10 +41,10 @@ class DynamicDataGen(DatasetList):
                         for ret in self._build_combinations(name, stri, v):
                             list_[i] = ret.copy()
                             yield self._merge_dictionaries(dic, {key: list_})
-                    else:
+                    elif ignore_var not in list_[i]:
                         list_[i] = stri
                         yield self._merge_dictionaries(dic, {key: list_})
-            else:
+            elif ignore_var not in val:
                 yield self._merge_dictionaries(dic, {key: stri})
 
     def _merge_dictionaries(self, x, y):
@@ -48,10 +52,10 @@ class DynamicDataGen(DatasetList):
         z.update(y)
         return z
 
-    def _build_xml_combinations(self, name, stri, ele):
-        if ele.text is not None:
+    def _build_xml_combinations(self, name, stri, ele, ignore_var):
+        if ele.text is not None and ignore_var not in ele.text:
             yield self._update_element(ele, stri)
-        for i, element in enumerate(ele.getchildren()):
+        for i, element in enumerate(list(ele)):
             for ret in self._build_xml_combinations(name, stri, element):
                 list_ = [_ for _ in ele.getchildren()]
                 list_[i] = ret.copy()
