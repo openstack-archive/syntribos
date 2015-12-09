64244b572a441fbc4b508f694800d1f212fe471c
michael.dong@rackspace.com <michaeldong1@gmail.com>
2015-12-09 10:27:34 -0600

    Added in issues for test results
    This change adds in the notion of an Issue class, which is intended
    to be an object that holds the data for vulnerabilities, rather than
    just storing the stack trace as we do currently
    
    Change-Id: Id47e5110c93acdeb1687fb9f6879b8254e5579ba

---
+++ syntribos/issue.py
@@ -0,0 +1,58 @@
+class Issue(object):
+    def __init__(self, severity, test="", text="",
+                 request=None, response=None, assertions=[]):
+        self.test = test
+        self.severity = severity
+        self.text = text
+        self.request = request
+        self.response = response
+        self.assertions = assertions
+        self.failure = False
+
+    def as_dict(self):
+        '''Convert the issue to a dict of values for outputting.'''
+        out = {
+            'test_name': self.test,
+            'issue_severity': self.severity,
+            'issue_text': self.text,
+            'request': self.request_as_dict(self.request),
+            'response': self.response_as_dict(self.response)
+        }
+        return out
+
+    def request_as_dict(self, req):
+        return {
+            'url': req.path_url,
+            'method': req.method,
+            'headers': dict(req.headers),
+            'body': req.body,
+            'cookies': req._cookies.get_dict()
+        }
+
+    def response_as_dict(self, res):
+        return {
+            'status_code': res.status_code,
+            'reason': res.reason,
+            'url': res.url,
+            'headers': dict(res.headers),
+            'cookies': res.cookies.get_dict(),
+            'text': res.text
+        }
+
+    def add_test(self, assertion, *args):
+        '''
+        Assertions will be stored as (assertion, arguments) tuples,
+        such as (assertTrue, resp.status != 500) or
+        (assertNotIn, line, resp.content)
+        '''
+        self.assertions.append((assertion,) + args)
+
+    def run_tests(self):
+        try:
+            for assertion in self.assertions:
+                assertion_function = assertion[0]
+                condition = assertion[1:]
+                assertion_function(*condition)
+        except AssertionError as e:
+            self.failure = True
+            raise e
