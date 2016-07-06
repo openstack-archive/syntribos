77699b1f960265b860cf746fbb268ad15ad9cd87
michael.dong@rackspace.com <michael.dong@rackspace.com>
2016-07-06 12:04:51 -0500

    Removed openCAFE dependencies from identity extension
    Change-Id: Iec38c57673b1b48818c26ae2ff19ba81c9057d1f

--- syntribos/extensions/identity/models/base.py
+++ syntribos/extensions/identity/models/base.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import json
+import logging
 import xml.etree.ElementTree as ET
 
-import cafe.engine.models.base
 import six
 
 
@@ -25,14 +25,43 @@ class Namespaces(object):
     XMLNS_RAX_AUTH = "http://docs.rackspace.com/identity/api/ext/RAX-AUTH/v1.0"
 
 
-class BaseIdentityModel(cafe.engine.models.base.AutoMarshallingModel):
+class BaseIdentityModel(object):
     _namespaces = Namespaces
 
     def __init__(self, kwargs):
         super(BaseIdentityModel, self).__init__()
+        self._log = logging.getLogger(__name__)
         for k, v in kwargs.items():
             if k != "self" and not k.startswith("_"):
                 setattr(self, k, v)
+
+    def serialize(self, format_type):
+        try:
+            serialize_method = '_obj_to_{0}'.format(format_type)
+            return getattr(self, serialize_method)()
+        except Exception as serialization_exception:
+            self._log.error(
+                'Error occured during serialization of a data model into'
+                'the "{0}: \n{1}" format'.format(
+                    format_type, serialization_exception))
+            self._log.exception(serialization_exception)
+
+    @classmethod
+    def deserialize(cls, serialized_str, format_type):
+        if serialized_str and len(serialized_str) > 0:
+            try:
+                deserialize_method = '_{0}_to_obj'.format(format_type)
+                return getattr(cls, deserialize_method)(serialized_str)
+            except Exception as deserialization_exception:
+                cls._log.exception(deserialization_exception)
+                cls._log.debug(
+                    "Deserialization Error: Attempted to deserialize type"
+                    " using type: {0}".format(format_type.decode(
+                        encoding='UTF-8', errors='ignore')))
+                cls._log.debug(
+                    "Deserialization Error: Unble to deserialize the "
+                    "following:\n{0}".format(serialized_str.decode(
+                        encoding='UTF-8', errors='ignore')))
 
     @classmethod
     def _remove_xml_namespaces(cls, element):
@@ -66,6 +95,14 @@ class BaseIdentityModel(cafe.engine.models.base.AutoMarshallingModel):
         element = self._obj_to_xml_ele()
         element.attrib["xmlns"] = self._namespaces.XMLNS
         return ET.tostring(element)
+
+    # These next two functions must be defined by the child classes before
+    # serializing
+    def _obj_to_dict(self):
+        raise NotImplementedError
+
+    def _obj_to_xml_ele(self):
+        raise NotImplementedError
 
     @staticmethod
     def _find(element, tag):
